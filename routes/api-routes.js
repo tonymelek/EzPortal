85e4653dc923b6874d3831a2d987a52afe8e030e
Tony Melek <tonymelek.au@gmail.com>
2020-11-10 22:39:54 +1100

    initial commit

---
+++ routes/api-routes.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+///use router instead of app when creating the routes
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,32 @@
+//Call/Import  Deoendencies
+const express = require('express')
+const path = require('path')
+const expbs = require('express-handlebars')
+const db = require('./models')
+const app = express(); //Start Express
+const PORT = process.env.PORT || 5000; //Set Port
+
+//Identify the use of Handlebars
+app.engine('handlebars', expbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars')
+
+//Handle Post Response
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Satatic Route
+app.use(express.static(path.join(__dirname, 'public')));
+//html-routes
+app.use(require('./routes/html-routes'))
+app.use('/api', require('./routes/api-routes'))
+
+
+
+
+
+
+//Initialise server
+db.sequelize.sync().then(() => {
+    app.listen(PORT, () => console.log(`The Express Server is now Up and running on PORT : ${PORT}`))
+})
+
